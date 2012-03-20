e31e3e9f493e0b2d673fee030d2a00ad94c55bda
Anthony Young <sleepsonthefloor@gmail.com>
2012-03-20 13:05:15 -0700

    update comment

--- animals/cats/views.py
+++ animals/cats/views.py
@@ -51,7 +51,7 @@ class DetailView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(DetailView, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
+        # Wait!  Something is amiss!
         context['cat'] = {'thumb': ('http://maxcdn.liewcf.com/blog/wp-content'
                                     '/uploads/mini-dog-pic11236.jpg'),
                           'id': 3,
