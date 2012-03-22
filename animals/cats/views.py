149a6b2a13e029549f05d936a8816015999b9415
Anthony Young <sleepsonthefloor@gmail.com>
2012-03-21 22:29:16 -0700

    update image link

--- animals/cats/views.py
+++ animals/cats/views.py
@@ -50,8 +50,8 @@ class DetailView(generic.TemplateView):
         # Call the base implementation first to get a context
         context = super(DetailView, self).get_context_data(**kwargs)
         # Wait!  Something is amiss!
-        context['cat'] = {'thumb': ('http://maxcdn.liewcf.com/blog/wp-content'
-                                    '/uploads/mini-dog-pic11236.jpg'),
+        context['cat'] = {'thumb': ('http://images.ansolabs.com/'
+                                    'mini-dog-pic11236.jpg'),
                           'id': 3,
                           'type': 'cute',
                           'url': 'http://www.liewcf.com/cute-mini-dog-1009/'}
